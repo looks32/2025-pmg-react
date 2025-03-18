4797a50d2b7b7845f3a2169457b1c61e7189a30c
Dang <wheodnjs2@naver.com>
2025-03-18 11:18:04 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Join
+# Login
+# Board
---
+++ app/page.js
@@ -0,0 +1,10 @@
+import styles from "./css/page.module.css";
+
+export default function Home() {
+  return (
+    <div>
+      <main className={styles.main}>main</main>
+      <footer className={styles.footer}>footer</footer>
+    </div>
+  );
+}
