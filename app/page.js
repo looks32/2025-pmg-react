01f53269c85283f6b6ebc99c2347e1279a80e046
kimdaae328 <kimdaae328@gmail.com>
2025-03-18 13:13:26 +0900

    헤더 추가

--- app/page.js
+++ app/page.js
@@ -3,6 +3,7 @@ import styles from "./css/page.module.css";
 export default function Home() {
   return (
     <div>
+      <header className={styles.header}>header</header>
       <main className={styles.main}>main</main>
       <footer className={styles.footer}>footer</footer>
     </div>
