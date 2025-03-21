dc51e57408352f4fc12493ab8adc419b86795a69
kimdaae328 <kimdaae328@gmail.com>
2025-03-21 13:27:03 +0900

    input, btn 추가

--- app/login/page.js
+++ app/login/page.js
@@ -1,7 +1,14 @@
+import Btn from "@/components/btn";
+import Input from "@/components/input";
 import styles from "@/css/login/login.module.css";
 
 export default function Login() {
   return (
-    <div className={styles.login}>로그인</div>
+    <div>
+      <div className={styles.login}>로그인</div>
+      <Input placeholder="아이디" error="아이디를 입력해주세요"/>
+
+      <Btn text="확인"/>
+    </div>
   );
 }
--- components/input.js
+++ components/input.js
@@ -1,7 +1,10 @@
 import styles from "@/css/components/input.module.css";
 
-export default function Input({placeholder}) {
+export default function Input({placeholder, error}) {
     return (
-        <input placeholder={placeholder} className={styles.input}></input>
+        <div className={styles.inputWrap}>
+            <input placeholder={placeholder} className={styles.input}/>
+            {error && <p className={styles.errorText}>{error}</p>}
+        </div>
     );
 }
