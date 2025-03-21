88475c35d3077cf08290f6e094efd57ec4ef3352
kimdaae328 <kimdaae328@gmail.com>
2025-03-21 14:22:43 +0900

    가이드 추가

---
+++ app/guide/page.js
@@ -0,0 +1,28 @@
+'use client';
+import Btn from "@/components/btn";
+import Input from "@/components/input";
+import TitleText from "@/components/titleText";
+
+export default function Board() {
+  return (
+    <>
+      <h2>타이틀</h2>
+      <TitleText text="제목"/>
+
+      <h2>버튼</h2>
+      <Btn text="버튼"/>
+      <Btn text="버튼" line/>
+      <Btn text="버튼" fullWidth/>
+
+      <h2>인풋</h2>
+      <Input placeholder="내용을 입력해주세요" error="내용을 입력하지 않았습니다."/>
+
+      <style jsx>{`
+        h2 {
+          font-size: 18px;
+          margin: 80px 0 20px;
+        }
+      `}</style>
+    </>
+  );
+}
