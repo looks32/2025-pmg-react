927125532d45057d8809f71be3a3c4638a359e18
Dang <wheodnjs2@naver.com>
2025-03-21 14:13:56 +0900

    fontawesome 추가

--- app/board/page.js
+++ app/board/page.js
@@ -1,12 +1,16 @@
 import Btn from "@/components/btn";
 import TitleText from "@/components/titleText";
 
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faArrowRight } from "@fortawesome/free-solid-svg-icons/faArrowRight";
+
 export default function Board() {
   return (
     <>
       <TitleText text="제목"/>
       <div>board</div>
       <Btn text="게시판 버튼"/>
+      <FontAwesomeIcon icon={faArrowRight} />
     </>
   );
 }
