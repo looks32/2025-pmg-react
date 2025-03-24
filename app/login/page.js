e2fda925fa33c9a977628f23c78053a0ca5fcc4f
kimdaae328 <kimdaae328@gmail.com>
2025-03-24 13:33:47 +0900

    아이디찾기 페이지 추가

--- app/login/page.js
+++ app/login/page.js
@@ -2,16 +2,30 @@ import Btn from "@/components/btn";
 import Input from "@/components/input";
 import TitleText from "@/components/titleText";
 import styles from "@/css/login/login.module.css";
+import Link from "next/link";
 
 export default function Login() {
   return (
     <div className={styles.loginContent}>
       <div>
-        <TitleText text="로그인"/>
-        <Input placeholder="아이디"/>
-        <Input placeholder="비밀번호"/>
+        <div className={styles.loginForm}>
+          <TitleText text="로그인"/>
+          <Input placeholder="아이디"/>
+          <Input placeholder="비밀번호"/>
 
-        <Btn text="확인" fullWidth/>
+          <Btn text="로그인" fullWidth/>
+        </div>
+        <ul className={styles.loginInfo}>
+          <li>
+            <Link href="">아이디 찾기</Link>
+          </li>
+          <li>
+            <Link href="">비밀번호 찾기</Link>
+          </li>
+          <li>
+            <Link href="">회원가입</Link>
+          </li>
+        </ul>
       </div>
     </div>
   );
