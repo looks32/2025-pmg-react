6dedb440135a88338021c36a54abd10e654d3301
kimdaae328 <kimdaae328@gmail.com>
2025-03-24 16:28:49 +0900

    비밀번호 찾기 추가

--- components/complete.js
+++ components/complete.js
@@ -1,10 +1,15 @@
+import Btn from "@/components/btn";
 import styles from "@/css/components/complete.module.css";
 
 export default function Complete({ id, password, showId, showPassword }) {
   return (
     <div className={styles.completeWrap}>
+      <div className={styles.complteArea}>
         {showId && <p>아이디는 <span>{id}</span>입니다.</p>}
         {showPassword && <p>비밀번호는 <span>{password}</span>입니다.</p>}
+      </div>
+
+      <Btn text="로그인" />
     </div>
   )
 }
