51c7771b55d7da90da17966c06051db5b23188a7
Dang <wheodnjs2@naver.com>
2025-03-24 16:21:15 +0900

    게시판 router, pagination

---
+++ app/board/[id]/page.js
@@ -0,0 +1,14 @@
+// import Btn from "@/components/btn";
+// import TitleText from "@/components/titleText";
+// import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+// import { faThumbsUp } from "@fortawesome/free-solid-svg-icons/faThumbsUp";
+// import { faThumbsDown } from "@fortawesome/free-solid-svg-icons/faThumbsDown";
+
+
+export default function BoardDetail({params: { id }}) {
+  return (
+    <>
+      <div>BoardDetail {id}</div>
+    </>
+  );
+}
--- app/board/page.js
+++ app/board/page.js
@@ -3,16 +3,24 @@ import TitleText from "@/components/titleText";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faThumbsUp } from "@fortawesome/free-solid-svg-icons/faThumbsUp";
 import { faThumbsDown } from "@fortawesome/free-solid-svg-icons/faThumbsDown";
+import Link from "next/link";
+import Pagination from "@/components/pagination";
 
 
 export default function Board() {
   return (
     <>
-      <TitleText text="제목"/>
+      <TitleText text="일반 게시판"/>
       <div>board</div>
-      <Btn text="게시판 버튼"/>
-      <FontAwesomeIcon icon={faThumbsUp} style={{color:"#c0c0c0"}}/>
+
+      <Link href="/board/123">링크 123</Link>
+
+      <Btn text="글쓰기"/>
+
+      <FontAwesomeIcon icon={faThumbsUp} style={{color:"#c0c0c0"}} />
       <FontAwesomeIcon icon={faThumbsDown} style={{color: "#c0c0c0"}} />
+
+      <Pagination/>
     </>
   );
 }
