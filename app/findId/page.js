97d0b41936f896eb3aabba859d18a18fdaf6f324
kimdaae328 <kimdaae328@gmail.com>
2025-03-24 13:33:59 +0900

    아이디찾기 페이지 추가

--- app/findId/page.js
+++ app/findId/page.js
@@ -1,7 +1,21 @@
-import React from 'react'
+import Btn from "@/components/btn";
+import Complete from "@/components/complete";
+import Input from "@/components/input";
+import TitleText from "@/components/titleText";
+import styles from "@/css/findId/findId.module.css";
 
 export default function findId() {
   return (
-    <div>findId</div>
+    <>
+        <TitleText text="아이디 찾기"/>
+
+        <Input placeholder="이메일"/>
+        <div className={styles.btnWrap}>
+            <Btn text="확인"/>
+        </div>
+
+        <Complete id="hello123" showId={true} showPassword={false} />
+        <Complete password="pass456" showId={false} showPassword={true} />
+    </>
   )
 }
---
+++ components/complete.js
@@ -0,0 +1,10 @@
+import styles from "@/css/components/complete.module.css";
+
+export default function Complete({ id, password, showId, showPassword }) {
+  return (
+    <div className={styles.completeWrap}>
+        {showId && <p>아이디는 <span>{id}</span>입니다.</p>}
+        {showPassword && <p>비밀번호는 <span>{password}</span>입니다.</p>}
+    </div>
+  )
+}
