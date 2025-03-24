9a7684e126cf078ce759ebdd2bb6d6fd2b9713c9
kimdaae328 <kimdaae328@gmail.com>
2025-03-24 16:42:35 +0900

    버튼 삭제

--- components/complete.js
+++ components/complete.js
@@ -4,12 +4,8 @@ import styles from "@/css/components/complete.module.css";
 export default function Complete({ id, password, showId, showPassword }) {
   return (
     <div className={styles.completeWrap}>
-      <div className={styles.complteArea}>
-        {showId && <p>아이디는 <span>{id}</span>입니다.</p>}
-        {showPassword && <p>비밀번호는 <span>{password}</span>입니다.</p>}
-      </div>
-
-      <Btn text="로그인" />
+      {showId && <p>아이디는 <span>{id}</span>입니다.</p>}
+      {showPassword && <p>비밀번호는 <span>{password}</span>입니다.</p>}
     </div>
   )
 }
