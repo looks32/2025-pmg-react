cc622f6345f997f453e5bcde63ef81eeaf2d192f
Dang <56210432+looks32@users.noreply.github.com>
2025-03-26 22:36:39 +0900

    write 추가, globals.css 수정

---
+++ app/board/[id]/write/page.js
@@ -0,0 +1,14 @@
+// import Btn from "@/components/btn";
+// import TitleText from "@/components/titleText";
+// import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+// import { faThumbsUp } from "@fortawesome/free-solid-svg-icons/faThumbsUp";
+// import { faThumbsDown } from "@fortawesome/free-solid-svg-icons/faThumbsDown";
+
+
+export default function BoardWrite({params: { id }}) {
+  return (
+    <>
+      <div>BoardWrite {id}</div>
+    </>
+  );
+}
