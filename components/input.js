18a7c1d8fde021d4af0d6646d737073027f83750
kimdaae328 <kimdaae328@gmail.com>
2025-04-10 10:26:15 +0900

    인풋 vlaue 추가

--- components/input.js
+++ components/input.js
@@ -1,9 +1,9 @@
 import styles from "@/css/components/input.module.css";
 
-export default function Input({name, type, placeholder, required, error}) {
+export default function Input({name, type, placeholder, required, value, error}) {
     return (
         <div className={styles.inputWrap}>
-            <input name={name} type={type} placeholder={placeholder} required={required} className={styles.input}/>
+            <input name={name} type={type} placeholder={placeholder} required={required} value={value} className={styles.input}/>
             {error && <p className={styles.errorText}>{error}</p>}
         </div>
     );
