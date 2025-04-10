8bf1de3b7af31dc0af42a931381fe44f18947605
kimdaae328 <kimdaae328@gmail.com>
2025-04-10 10:47:31 +0900

    인풋 수정

--- components/input.js
+++ components/input.js
@@ -1,9 +1,9 @@
 import styles from "@/css/components/input.module.css";
 
-export default function Input({name, type, placeholder, required, value, error}) {
+export default function Input({name, type, placeholder, required, value, onChange, error}) {
     return (
         <div className={styles.inputWrap}>
-            <input name={name} type={type} placeholder={placeholder} required={required} value={value} className={styles.input}/>
+            <input name={name} type={type} placeholder={placeholder} required={required} value={value ?? undefined} onChange={onChange} className={styles.input}/>
             {error && <p className={styles.errorText}>{error}</p>}
         </div>
     );
